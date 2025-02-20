a64eac0203358511567aa33c43f0a80fb334ba92
MEGA <MEGA@gmail.com>
2025-02-20 18:43:31 +0200

    Add project files.

---
+++ zad20.02/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace zad20._02
+{
+    internal class Program
+    {
+
+        static void InsertionSort(List<Student> students)
+        {
+            for (int i = 1; i < students.Count; i++)
+            {
+                var key = students[i];
+                int j = i - 1;
+
+                while (j >= 0 && (students[j].AvgGrade > key.AvgGrade || (students[j].AvgGrade == key.AvgGrade && string.Compare(students[j].Name, key.Name) > 0)))
+                {
+                    students[j + 1] = students[j];
+                    j--;
+                }
+                students[j + 1] = key;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            var students = new List<Student>
+        {
+            new Student("Елинор", 5.50),
+            new Student("Живко", 4.80),
+            new Student("Виктория", 6.00),
+            new Student("Иван", 5.50),
+            new Student("Максим", 5.00)
+        };
+
+            InsertionSort(students);
+
+            foreach (var student in students)
+            {
+                Console.WriteLine(student);
+            }
+        }
+    }
+}
---
+++ zad20.02/Student.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zad20._02
+{
+    internal class Student
+    {
+        public string Name { get; set; }
+        public double AvgGrade { get; set; }
+
+        public Student(string name, double avgGrade)
+        {
+            Name = name;
+            AvgGrade = avgGrade;
+        }
+
+        public override string ToString()
+        {
+            return ($"Student {Name} with average grade {AvgGrade}");
+        }
+
+    }
+}
